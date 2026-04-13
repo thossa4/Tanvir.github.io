5bf5370df5d82d12c8e490c2d29566ceee3a8590
thossa4 <thossa4@lsu.edu>
2026-04-13 01:58:51 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,8 +1,12 @@
 var map = L.map('map').setView([38, -95], 4);
 
-var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var basemap = L.tileLayer(basemapUrl).addTo(map);
+// Different basemap than OpenStreetMap
+var basemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}';
+var basemap = L.tileLayer(basemapUrl, {
+  attribution: 'Tiles © Esri'
+}).addTo(map);
 
+// Radar layer
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
   layers: 'nexrad-n0r-900913',
@@ -12,13 +16,28 @@ var radarDisplayOptions = {
 
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 
+// Alerts layer
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
+
 $.getJSON(weatherAlertsUrl, function(data) {
   L.geoJSON(data, {
     style: function(feature) {
+      var severity = feature.properties.severity;
       var alertColor = 'orange';
-      if (feature.properties.severity === 'Severe') alertColor = 'red';
-      return { color: alertColor };
+
+      if (severity === 'Severe') {
+        alertColor = 'red';
+      } else if (severity === 'Extreme') {
+        alertColor = 'purple';
+      } else if (severity === 'Minor') {
+        alertColor = 'yellow';
+      }
+
+      return {
+        color: alertColor,
+        weight: 2,
+        fillOpacity: 0.25
+      };
     },
     onEachFeature: function(feature, layer) {
       layer.bindPopup(feature.properties.headline);
